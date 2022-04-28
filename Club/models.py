8fb43d80a308d1025967909a27d171007429203d
laskscc <louis.ash-kaufman@seattlecolleges.edu>
2022-04-28 14:38:08 -0700

    models commit

--- Club/models.py
+++ Club/models.py
@@ -1,3 +1,55 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Meeting(models.Model):
+    meetingTitle=models.CharField(max_length=255)
+    meetingDate=models.DateField()
+    meetingTime=models.TimeField()
+    meetingLocation=models.CharField(max_length=255)
+    meetingAgenda=models.TextField()
+
+    def __str__(self):
+        return self.meetingTitle
+
+    class Meta:
+        db_table='meeting'
+
+class MeetingMinutes(models.Model):
+    meetingID = models.ForeignKey(Meeting, on_delete=models.DO_NOTHING)
+    attendance = models.ManyToManyField(User)
+    minutes = models.TextField()
+
+    def __str__(self):
+        return self.meetingID
+        
+    class Meta:
+        db_table='meetingMinutes'
+
+class Resource(models.Model):
+    resourceName = models.CharField(max_length=255)
+    resourceType = models.CharField(max_length=255)
+    resourceURL = models.URLField()
+    dateEntered = models.DateField()
+    posterUser = models.ForeignKey(User, on_delete=models.CASCADE)
+    resourceDescription = models.TextField()
+
+    def __str__(self):
+        return self.resourceName
+        
+    class Meta:
+        db_table='resource'
+
+class Event(models.Model):
+    eventTitle = models.CharField(max_length=255)
+    eventLocation = models.CharField(max_length=255)
+    eventDate = models.DateField()
+    eventTime = models.TimeField()
+    eventDescription = models.TextField()
+    posterUser = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.eventTitle
+        
+    class Meta:
+        db_table='event'
