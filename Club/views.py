9e2113decb4d7bc3db49a8c836518b0797a2868b
laskscc <louis.ash-kaufman@seattlecolleges.edu>
2022-05-01 23:26:19 -0700

    resource view

--- Club/views.py
+++ Club/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Resource
 
 # Create your views here.
 def index(request): 
     return render(request,  'Club/index.html')
+
+def resources(request):
+    resource_list=Resource.objects.all()#bad for big lists, use find instead
+    return render(request, 'Club/resources.html', {'resource_list': resource_list})
