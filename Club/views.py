4eb07320766d5f9d542581f4d3357e48b554b0f3
laskscc <louis.ash-kaufman@seattlecolleges.edu>
2022-05-04 12:50:37 -0700

    Meeting & Meeting detail view added

--- Club/views.py
+++ Club/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Resource
+from django.shortcuts import render, get_object_or_404
+from .models import Resource, Meeting, MeetingMinute
 
 # Create your views here.
 def index(request): 
@@ -8,3 +8,22 @@ def index(request):
 def resources(request):
     resource_list=Resource.objects.all()#bad for big lists, use find instead
     return render(request, 'Club/resources.html', {'resource_list': resource_list})
+
+def meetings(request):
+    meeting_list=Meeting.objects.all()
+    return render(request, 'Club/meetings.html', {'meeting_list':meeting_list})
+
+def meetingDetails(request, id):
+    meeting=get_object_or_404(Meeting, pk=id)
+    # date=meeting.meetingDate
+    # time=meeting.meetingTime
+    # locat=meeting.meetingLocation
+    minute=MeetingMinute.objects.get(meetingID=meeting.id)
+    context={
+        'meeting' : meeting,
+        # 'date' : date,
+        # 'time' : time,
+        # 'locat' : locat,
+        'minute': minute,
+    }
+    return render(request, 'Club/meetingDetails.html', {'meeting': meeting, 'minute' : minute})
